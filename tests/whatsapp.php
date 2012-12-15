19857a2162e1382cd86f336ec62033d78b96aa9e
ahmadtaleb <sixxman90@yahoo.com>
2012-12-15 04:50:34 +0300

    Update tests/whatsapp.php

--- tests/whatsapp.php
+++ tests/whatsapp.php
@@ -14,8 +14,8 @@ function fgets_u($pStdn) {
 }
 
 $nickname = "WhatsAPI Test";
-$sender = 	""; // Mobile number with country code (but without + or 00)
-$imei = 	""; // MAC Address for iOS IMEI for other platform (Android/etc) 
+$sender =962798147123""; // Mobile number with country code (but without + or 00)
+$imei =354913053272213""; // MAC Address for iOS IMEI for other platform (Android/etc) 
 
 
 $countrycode = substr($sender, 0, 2);
